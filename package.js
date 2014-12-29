193ada5b3b6ba8ad2d6dacf3b09d32c95dfe5439
Morten Henriksen <mh@gi2.dk>
2014-12-29 18:55:55 +0100

    2.3.2 - correct sound names

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:push',
-  version: '2.3.1',
+  version: '2.3.2',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
