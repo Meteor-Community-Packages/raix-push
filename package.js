80aefd90acd09e8ec6eed473f7ffc59457abf459
Morten Henriksen <mh@gi2.dk>
2015-01-27 20:08:12 +0100

    Use updated pushplugin

--- package.js
+++ package.js
@@ -13,8 +13,10 @@ Npm.depends({
 });
 
 Cordova.depends({
+  // Fix ios 7 and ios in general
+  'com.phonegap.plugins.PushPlugin': 'https://github.com/raix/PushPlugin/tarball/c4e3aa69c66bde45472e81ac303a9e39020c9cc7'
   // Fix issue 365 ios 7 missing badge updates
-  'com.phonegap.plugins.PushPlugin': 'https://github.com/raix/PushPlugin/tarball/ff4ade868488ef0fcb014da652681011cd95d8ea'
+  // 'com.phonegap.plugins.PushPlugin': 'https://github.com/raix/PushPlugin/tarball/ff4ade868488ef0fcb014da652681011cd95d8ea'
   // 'com.clone.phonegap.plugins.pushplugin': '2.4.1' //with #354 fixed OK
   //'com.phonegap.plugins.PushPlugin': 'http://github.com/rossmartin/PushPlugin/tarball/6cf2e1a107310e859839fb7a0dc2618a7a199430'
 });
