064677e6ff0da9bfa7c830b6ec2987176198ccb5
Alex Corre <alex@workpop.com>
2015-01-29 13:48:06 -0800

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'workpop:push',
-  version: '2.6.2',
+  version: '2.6.3',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
