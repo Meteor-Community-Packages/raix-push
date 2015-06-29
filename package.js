bd6b23270d234533c7623e7547a7c5cfc96e975f
Morten Henriksen <mh@gi2.dk>
2015-06-29 13:42:50 +0200

    Bump 2.6.11
    cc: @timbotnik

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:push',
-  version: '2.6.10',
+  version: '2.6.11',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
