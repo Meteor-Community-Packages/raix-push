c1602dddd9c159d4c8aeb84e53d4118b805f354c
Morten Henriksen <mh@gi2.dk>
2015-10-06 19:38:28 +0200

    version 3.0.1
    @jraede thanks - note that you can set the `sendInterval=null` in the
    `Push.Configure` to hinder the sender server to startup.
    
    Also note that the `Push.enabled()` works as default with `serverSend`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:push',
-  version: '3.0.0',
+  version: '3.0.1',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
