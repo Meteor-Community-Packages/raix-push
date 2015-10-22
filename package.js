d17e1eb26ef03dab233a3f9170c876db952c968f
Morten Henriksen <mh@gi2.dk>
2015-10-22 08:14:30 +0200

    Version 3.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:push',
-  version: '3.0.1',
+  version: '3.0.2',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
