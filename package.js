0f8e511839d961db981bdf826e25a1b6cc15c41f
Morten Henriksen <mh@gi2.dk>
2016-04-20 14:09:22 +0200

    Add device dependency and deprecate json config

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:push',
-  version: '3.0.2',
+  version: '3.0.3_rc.1',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
@@ -12,7 +12,8 @@ Npm.depends({
 });
 
 Cordova.depends({
-  'phonegap-plugin-push': '1.3.0'
+  'phonegap-plugin-push': '1.3.0',
+  'cordova:cordova-plugin-device': '1.1.1',
 });
 
 Package.registerBuildPlugin({
