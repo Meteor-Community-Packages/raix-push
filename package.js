7559d7a7f58c4a2ffaa1a05756c335a212814695
Morten Henriksen <mh@gi2.dk>
2016-05-02 12:12:14 +0200

    release 3.0.3-rc.2
    Bumped the cordova plugin to 1.6.3 - this might fix dex errors while
    building on meteor 1.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:push',
-  version: '3.0.3-rc.1',
+  version: '3.0.3-rc.2',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
@@ -12,7 +12,7 @@ Npm.depends({
 });
 
 Cordova.depends({
-  'phonegap-plugin-push': '1.3.0',
+  'phonegap-plugin-push': '1.6.3', // 1.3.0
   'cordova:cordova-plugin-device': '1.1.1',
 });
 
