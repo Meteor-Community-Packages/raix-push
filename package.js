309e578af278022bd0ef09603121099ab4db119c
Mihailo Jovković <mjovkovic@gmail.com>
2018-11-28 15:47:08 +0100

    Fixed issue with test config.

--- package.js
+++ package.js
@@ -94,6 +94,29 @@ Package.onTest(function (api) {
     'ejson'
   ]);
 
+  // API
+  api.addFiles('lib/client/cordova.js', 'web.cordova');
+
+  // Common api
+  api.addFiles([
+    'lib/common/main.js'
+  ], ['web.browser', 'server']);
+
+  // Common api
+  api.addFiles([
+    'lib/common/notifications.js'
+  ], ['client', 'server']);
+
+  // API's
+  api.addFiles('lib/client/browser.js', 'web.browser');
+  api.addFiles('lib/server/push.api.js', 'server');
+  api.addFiles('lib/server/push.api.legacy.js', 'server');
+
+  // // Unified api
+  api.addFiles('lib/client/client.js', 'client');
+  api.addFiles('lib/server/server.js', 'server');
+  api.addFiles('lib/server/server.legacy.js', 'server');
+
   // Finally add an entry point for tests
   api.mainModule('lib/server/push.api.tests.js');
 });
